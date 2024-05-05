5f890e3d1d560257f8c1bc9fc368e4e6bd08243b
Naeem009 <m.n.shahzad@gmail.com>
2024-05-05 16:19:51 +0500

    Some changes in CSS and others

--- src/app/about/about.tsx
+++ src/app/about/about.tsx
@@ -3,7 +3,21 @@ import Link from "../../../node_modules/next/link";
 export default function About() {
   return (
     <div> 
-    <h1>About us</h1>
+        <section>
+        <h1>About Us</h1>
+    </section>
+    <section className="about-us">
+        <div className="row">
+            <div className="about-col">
+                <h1>We are the world&aposs largest University</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore cumque in accusantium molestias maiores fuga vitae eos, ducimus officiis repudiandae voluptatem error. Laboriosam, numquam blanditiis aspernatur, nobis tempora consequatur id aliquam asperiores voluptatum iste animi voluptatem fugiat totam excepturi dolor.</p>
+                <Link href="#" className="hero_btn btn">EXPLORE NOW</Link>
+            </div>
+            <div className="about-col">
+                <img src="img/about.png" alt=""/>
+            </div>
+        </div>
+    </section>
     </div>
   )
 };
--- src/app/navigation/navigation.tsx
+++ src/app/navigation/navigation.tsx
@@ -3,18 +3,22 @@ import Link from "next/link";
 export default function Navigation() {
     return (
         <div>
-            <nav>
-                <Link href="/"className="logo">Xplore <i className="fab fa-staylinked"></i>kill</Link>
-                <div className="nav-links ml-0 mr-auto" id="navLinks">
-                    <ul>
-                        <li><Link href="/">Home</Link></li>
-                        {/* <li><Link href="/about">About</Link></li> */}
-                        <li><Link href="/course">Course</Link></li>
-                        <li><Link href="/blog">Blog</Link></li>
-                        <li><Link href="/contact">Contact</Link></li>
-                    </ul>
-                </div>
-            </nav>
-        </div>
+            <section>
+        <nav>
+            <Link href="/" className="logo">Xplore
+                <i className="fab fa-staylinked"></i>kill
+            </Link>
+            <div className="nav-links" id="navLinks">
+                <ul>
+                    <li><Link href="/">Home</Link></li>
+                    <li><Link href="course">Course</Link></li>
+                    <li><Link href="blog">Blog</Link></li>
+                    <li><Link href="about">About</Link></li>
+                    <li><Link href="contact">Contact</Link></li>
+                </ul>
+            </div>
+        </nav>
+        </section>
+        </div>        
     );
 }
